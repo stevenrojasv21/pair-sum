b9e982af06842c611931d2157cbd089978a9f8dc
stevenrojasv21 <24830207+stevenrojasv21@users.noreply.github.com>
2023-08-17 22:09:46 -0500

    Adding a comment explaining the algorithm

--- src/main.js
+++ src/main.js
@@ -13,8 +13,10 @@ export function pairSum(array, target) {
 
   // Since we iterate over the array only once, we have an O(n) complexity in execution and an O(n) in memory usage
   array.forEach((element) => {
+    // We need to know what other number we need to add up to get the target value, so, we store this result
+    // in an object (that behaves like a hash table, so, we have an access in O(1)) and if we find the same number later when 
+    // we continue iterating over the list, we will already know what the tuple is and push those two numbers into the tuples variable 
     const pair = target - element;
-    // We store this result in an object (that behaves like a hash table, so, we have an access in O(1))
     previousResults[pair] = element;
 
     // NOTE: let's suppose that the order of the tuples does not matter
